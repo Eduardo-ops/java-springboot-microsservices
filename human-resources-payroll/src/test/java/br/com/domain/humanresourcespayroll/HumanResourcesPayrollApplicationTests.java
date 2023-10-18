aaaa6a646aee8d6448a65369af6e0fbc54b22ab6
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-10-18 10:44:11 -0300

    Project human resources payroll created

---
+++ human-resources-payroll/src/test/java/br/com/domain/humanresourcespayroll/HumanResourcesPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanresourcespayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
