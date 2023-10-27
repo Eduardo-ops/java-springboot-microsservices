4209c2bfddf6ba8e7d5849c39fe9a29b7cb08cd3
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-10-27 15:23:59 -0300

    Configuring eureka server

--- human-resources-eureka-server/src/main/java/br/com/domain/humanresourceseurekaserver/HumanResourcesEurekaServerApplication.java
+++ human-resources-eureka-server/src/main/java/br/com/domain/humanresourceseurekaserver/HumanResourcesEurekaServerApplication.java
@@ -2,7 +2,9 @@ package br.com.domain.humanresourceseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HumanResourcesEurekaServerApplication {
 
