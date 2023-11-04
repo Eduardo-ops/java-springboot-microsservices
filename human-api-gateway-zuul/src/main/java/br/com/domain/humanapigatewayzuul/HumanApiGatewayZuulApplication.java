e9f338049159d8c0bfdd638ed3cd9e50d527b0ac
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-11-04 17:00:19 -0300

    Project api gateway zuul created

---
+++ human-api-gateway-zuul/src/main/java/br/com/domain/humanapigatewayzuul/HumanApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ human-api-gateway-zuul/src/test/java/br/com/domain/humanapigatewayzuul/HumanApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
