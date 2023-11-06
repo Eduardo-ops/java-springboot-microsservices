9409046cac70f2757216514c285a9407ba5736a9
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-11-06 19:16:58 -0300

    Project configuration server created

---
+++ human-resources-configuration-server/src/main/java/br/com/domain/humanresourcesconfigurationserver/HumanResourcesConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanresourcesconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ human-resources-configuration-server/src/test/java/br/com/domain/humanresourcesconfigurationserver/HumanResourcesConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanresourcesconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
