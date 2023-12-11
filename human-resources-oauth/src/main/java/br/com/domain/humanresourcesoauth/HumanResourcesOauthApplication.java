fda43efb10c685071e6e71d59f46c22f3699809f
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-12-11 19:01:38 -0300

    Created project human resources oauth

---
+++ human-resources-oauth/src/main/java/br/com/domain/humanresourcesoauth/HumanResourcesOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.domain.humanresourcesoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesOauthApplication.class, args);
+	}
+
+}
