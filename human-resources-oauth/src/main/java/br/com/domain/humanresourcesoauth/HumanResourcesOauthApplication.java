4bb648cb449b8abda120fdcd665a6faa0741b1eb
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-12-11 19:14:03 -0300

    Configuration of project oauth

--- human-resources-oauth/src/main/java/br/com/domain/humanresourcesoauth/HumanResourcesOauthApplication.java
+++ human-resources-oauth/src/main/java/br/com/domain/humanresourcesoauth/HumanResourcesOauthApplication.java
@@ -2,7 +2,9 @@ package br.com.domain.humanresourcesoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HumanResourcesOauthApplication {
 
